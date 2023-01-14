e42cf0756c3ac20b6f8fd30414b104eea12956c5
Lee  Chanho <sma05091@nate.com>
2023-01-14 22:49:34 +0900

    POST, Redirect GET

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -72,7 +73,7 @@ public class BasicItemController {
      * model.addAttribute(item); 자동 추가, 생략 가능
      * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
      */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
         itemRepository.save(item);
         return"basic/item";
@@ -87,6 +88,21 @@ public class BasicItemController {
         itemRepository.save(item);
         return "basic/item";
     }
+
+    //@PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return"redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        Item saveItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", saveItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return"redirect:/basic/items/{itemId}";
+    }
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
